def69670884fc20313fcc5de637e4cd87a3d3795
Mahmoud Medhat <M7moooud.17@gmail.com>
2024-05-22 01:34:55 +0300

    .

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const jsonServer = require("json-server");
-const server = create();
-const router = _router("db.json");
-const middleWare = defaults();
+import jsonServer from "json-server";
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middleWare = jsonServer.defaults();
+const port = 8000;
 
 
 server.use(middleWare);
 server.use(router);
-server.listen(process.env.PORT || 8000, ()=>{
-    console.log("Server is running!");
-});
+server.listen(port);
